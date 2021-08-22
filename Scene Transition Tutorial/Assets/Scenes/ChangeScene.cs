85ab0241b642b7f39b3f6ebd58d0576691c7cc13
MFAW28 <mfaisalaw2804@gmail.com>
2021-08-22 13:07:31 +0700

    Add 22 Agustus 2021

---
+++ Scene Transition Tutorial/Assets/Scenes/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public Animator animationScene;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animationScene.SetTrigger("SceneOut");
+            StartCoroutine(SceneTransition());
+        }
+    }
+
+    IEnumerator SceneTransition()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Scene2");
+    }
+}
